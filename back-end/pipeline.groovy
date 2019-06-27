4d3f6ad7d69fac9ada5afda21832bbb398ae4d34
Vanilson Santos <vanilsonsantos@MacBook-Pro.local>
2019-06-27 19:04:22 -0300

    Adds libraries

--- back-end/pipeline.groovy
+++ back-end/pipeline.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 
+import hudson.model.*
+import hudson.EnvVars
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import java.net.URL
+
 class Pipeline {
 
     def Pipeline() {}
