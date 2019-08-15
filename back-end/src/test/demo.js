a613676d5ca7c0ea64e0b3973b552492c115b591
Vanilson Santos <vanilsonsantos@MacBook-Pro-2.local>
2019-08-15 12:30:09 -0300

    Inlcudes tests for back-end and front-end

---
+++ back-end/src/test/demo.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+
+describe('Running tests with Mocha & Chai:', () => {
+  it('First test', () => {
+    assert.equal(true, true);
+  });
+});
---
+++ front-end/test/demo.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+
+describe('Running tests with Mocha & Chai:', () => {
+  it('Second test', () => {
+    assert.equal(false, true);
+  });
+});
